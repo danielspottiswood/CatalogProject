9fc89fde0822c69b9baac3d0605a0983254588e3
Daniel Spottiswood <Danielspottiswood@gmail.com>
2018-05-14 12:15:22 -0700

    Last formatting error

--- application.py
+++ application.py
@@ -120,7 +120,7 @@ def fbdisconnect():
     facebook_id = login_session['facebook_id']
     # The access token must me included to successfully logout
     access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?acce', \
+    url = 'https://graph.facebook.com/%s/permissions?acce' \
         'ss_token=%s' % (facebook_id, access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
